317452d8f8ca85f572058c943a32012fd6e18eb4
Matt C <matt@getcryst.al>
2022-08-29 22:42:29 -0400

    beginnings of a database and verifiable "account"

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import os
+import os,yaml
 
 class db:
 
@@ -11,3 +11,30 @@ class db:
         if os.path.exists(self.srcdir + os.sep + uid):
             return True
         return False
+
+    def make_user(self, uid, tz):
+        if self.check_user(uid):
+            return False
+        else:
+            obj = {
+                "tz": tz,
+                "watching": []
+            }
+            with open(self.srcdir + os.sep + uid, "w") as f:
+                f.write(yaml.dump(obj))
+            return True
+
+    def get_user(self, uid):
+        if self.check_user(uid):
+            with open(self.srcdir + os.sep + uid) as f:
+                obj = yaml.safe_load(f)
+            return obj
+        else:
+            return {"message": "not found"}
+
+if __name__ == "__main__":
+    db = db("db")
+    print(db.check_user("matt"))
+    db.make_user("matt", "est")
+    print(db.check_user("matt"))
+    print(str(db.get_user("matt")))
