95c9a65107e0a4da19920d45025e8ac8698cf30c
Matt C <matt@getcryst.al>
2022-08-30 17:27:21 -0400

    More progress on user module and web-functions

--- app.py
+++ app.py
@@ -1,31 +1,57 @@
-from flask import Flask,render_template,request,make_response,redirect
+from flask import Flask, render_template, request, make_response, redirect
 
 from datetime import datetime
 
+from data import db
+
 app = Flask(__name__)
+db = db("db")
 
 
 @app.route("/")
 def main():
+    p_content = ""
+    p_title = ""
+    extra = ""
+    clear_msg = False
+
     user = request.cookies.get('uid')
+    msg = request.cookies.get('msg')
+
+    if msg is not None:
+        extra = '<p style="color:red;">' + msg + "</p>"
+        clear_msg = True
 
     if user is None:
         p_title = "Home"
-        p_content = render_template("signin.html")
+        p_content = extra + render_template("signin.html")
     else:
         p_title = "Saved data for " + user
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        p_content = render_template("logout.html") + "<br/><p>Server time is: " + current_time + "</p>"
+        p_content = extra + render_template("logout.html") + "<br/><p>Server time is: " + current_time + "</p>"
+        p_content += "<h2>Your data:</h2><pre><code>" + str(db.get_user(user)) + "</code></pre>"
 
-    return render_template("page.html", page_title=p_title, content=p_content)
+    resp = make_response(render_template("page.html", page_title=p_title, content=p_content))
+
+    if clear_msg:
+        resp.delete_cookie('msg')
+
+    return resp
+
 
 @app.route("/dologin", methods=["POST"])
 def handle_login():
-    user = request.form['nm']
+    given_uid = request.form['nm']
     resp = make_response(redirect("/"))
-    resp.set_cookie('uid', user)
+
+    if db.check_user(given_uid):
+        resp.set_cookie('uid', given_uid)
+    else:
+        resp.set_cookie('msg', 'No such user ' + given_uid)
+
     return resp
+
 
 @app.route("/dologout", methods=["POST", "GET"])
 def handle_logout():
--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import os,yaml
 
+
 class db:
 
     def __init__(self, srcdir):
@@ -32,9 +33,25 @@ class db:
         else:
             return {"message": "not found"}
 
+    def add_watching(self, uid, who):
+        if self.check_user(uid):
+            user = self.get_user(uid)
+            user['watching'].append(who)
+            with open(self.srcdir + os.sep + uid, "w") as f:
+                f.write(yaml.dump(user))
+
+    def get_watching(self, uid):
+        if self.check_user(uid):
+            return self.get_user(uid)['watching']
+
+
 if __name__ == "__main__":
     db = db("db")
     print(db.check_user("matt"))
     db.make_user("matt", "est")
     print(db.check_user("matt"))
+    print(str(db.get_user("matt")))
+    db.add_watching("matt","joe")
+    print(str(db.get_user("matt")))
+    db.add_watching("matt", "mama")
     print(str(db.get_user("matt")))
